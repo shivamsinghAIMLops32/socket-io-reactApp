913a2fa5ad0977d42c75d98b2411f946cef3b8ed
shivamsinghAIMLops32 <136737397+shivamsinghAIMLops32@users.noreply.github.com>
2025-01-22 09:14:26 +0530

    change in cors

--- server/index.js
+++ server/index.js
@@ -11,7 +11,7 @@ const app = express();
 const server  = http.createServer(app);
 const io = new Server(server,{
     cors: {
-        origin: '*', // client url
+        origin: 'http://localhost:5173/', // client url
         methods: ['GET', 'POST', 'PUT'],
         credentials: true
     }
